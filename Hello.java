1d5e05d1164c4104a6ffe5fc1222d5ba22360eaa
Rama Ibseileh <ramaib@umich.edu>
2025-06-11 16:15:15 -0400

    Change in new feature branch

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for(int i=1; i<=10; i++) {
-            System.out.println("Hello Universe..." + i);  // <- changed "World" to "Universe"
+            System.out.println("Hello People..." + i);  // <- changed "World" to "Universe"
         }
     }
 }
